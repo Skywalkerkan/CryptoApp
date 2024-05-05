a1e7bfd8d6278d7bc4857f0dab444739e447394a
Skywalkerkan <cosarerkann>
2024-05-05 18:55:58 +0300

    Favorites Added

---
+++ KriptoApp/Extensions/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  KriptoApp
+//
+//  Created by Erkan on 5.05.2024.
+//
+
+import Foundation
---
+++ KriptoApp/Helper/AppColors.swift
@@ -0,0 +1,8 @@
+//
+//  AppColors.swift
+//  KriptoApp
+//
+//  Created by Erkan on 5.05.2024.
+//
+
+import Foundation
