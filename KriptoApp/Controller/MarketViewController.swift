88ce791120bd52fa9fa2067ad232823a7801a139
Skywalkerkan <cosarerkann>
2024-05-08 13:01:40 +0300

    SearchViewController and Main is handled

---
+++ KriptoApp/Controller/MarketViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MarketViewController.swift
+//  KriptoApp
+//
+//  Created by Erkan on 8.05.2024.
+//
+
+import UIKit
+
+class MarketViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
